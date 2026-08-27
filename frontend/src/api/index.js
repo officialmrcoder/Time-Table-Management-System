ef25a981b733346024353493f25a08e322f784df
officialmrcoder <officialmrcoder@gmail.com>
2026-08-27 23:54:36 +0500

    Initial commit

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:5000/api';
+const API_BASE_URL = 'https://time-table-management-system-lrpq.onrender.com';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
