d75f07fd8a20b8c387d99d8634989ae575da60f9
officialmrcoder <officialmrcoder@gmail.com>
2026-08-28 00:41:42 +0500

    Fix API URL to include /api

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://time-table-management-system-3.onrender.com';
+const API_BASE_URL = 'https://time-table-management-system-3.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
